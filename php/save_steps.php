49479156db7c721c3c1349e428288e154a073ad1
RusGameHack <81467467+RusGameHack@users.noreply.github.com>
2022-06-02 21:45:06 +0300

    Delete php directory

--- php/save_steps.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $filename = 'steps.txt';
-    $f=fopen($filename,'w');
-    fwrite($f,$_POST['step']);
-    fclose($f);
-?>
--- php/steps.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $filename = 'steps.txt';
-    $file = file_get_contents($filename);
-    echo json_encode($file);
-    //$file = $file - ($_POST['price']);
-    $f=fopen($filename,'w');
-    fwrite($f,$file);
-    fclose($f);
-?>
